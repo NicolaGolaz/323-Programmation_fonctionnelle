51bc8dae89e3051aa14a365968d37529f669e003
NicolaGolaz <nicola.golaz@eduvaud.ch>
2025-10-29 10:36:27 +0100

    Exercice fibo

---
+++ Nicola/Fibo/Fibo/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine(Fibonacci(-1));
+static int Fibonacci(int n)
+{
+    if (n < 0)
+    {
+        Console.WriteLine("pas de nombre négatif");
+        return n;
+    }
+    if (n < 2)
+        return n;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+    
+}
+
+for (int i = 0; i < 13; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
